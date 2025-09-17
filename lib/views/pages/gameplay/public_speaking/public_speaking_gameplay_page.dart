b991f1cf26dbf66a119436f7a81734680c15f877
InsaneCoder <kemdavid10@gmail.com>
2025-09-17 23:23:16 +0800

    feat: add public speaking gameplay page placeholder controls

--- lib/views/pages/gameplay/public_speaking/public_speaking_gameplay_page.dart
+++ lib/views/pages/gameplay/public_speaking/public_speaking_gameplay_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:voquadro/views/pages/gameplay/feedback_page.dart';
+
 class PublicSpeakingGameplayPage extends StatelessWidget {
   const PublicSpeakingGameplayPage({super.key});
 
@@ -10,18 +11,38 @@ class PublicSpeakingGameplayPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return FeedbackPage();
+            Container(
+              color: Colors.amberAccent,
+              width: 300,
+              height: 100,
+              child: Center(child: Text('Prompt')),
+            ),
+            Image.asset('assets/images/tempCharacter.png'),
+            Container(
+              color: Colors.amberAccent,
+              width: 200,
+              height: 50,
+              child: Center(child: Text('Timer')),
+            ),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  child: IconButton(
+                    onPressed: () {
+                      // Temporary Route to Feedback Page
+
+                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+                        return FeedbackPage();
+                      },));
                     },
+                    icon: Icon(Icons.mic),
+                    color: Colors.blue,
                   ),
-                );
-              },
-              child: Text('Finished Speaking'),
+                ),
+              ],
             ),
           ],
         ),
