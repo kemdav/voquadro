3a21e781979b45d8f4dcab9d169fb333c2f4353a
cozysharkmurks <wilfredalternate6969@gmail.com>
2025-10-09 21:45:14 +0800

    fix: resolve conflicts

--- lib/widgets/Profile/profile_edit_sheet.dart
+++ lib/widgets/Profile/profile_edit_sheet.dart
@@ -7,8 +7,6 @@ import 'package:voquadro/src/hex_color.dart';
 /// Parent responsibilities:
 /// - Provide `onPickAvatar` / `onPickBanner` to open pickers
 /// - Persist/save the `onSaveBio` result where desired
-///
-/// asked ai to add comments for the things kay gikapoy nako
 class ProfileEditSheet extends StatefulWidget {
   const ProfileEditSheet({
     super.key,
