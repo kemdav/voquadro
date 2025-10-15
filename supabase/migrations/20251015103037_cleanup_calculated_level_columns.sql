096d09a6a61d952b01462886ba393f8f11adf0a8
nasatap <exceljosephduran195@gmail.com>
2025-10-15 18:31:18 +0800

    fix: deleted levels in user table

---
+++ supabase/migrations/20251015103037_cleanup_calculated_level_columns.sql
@@ -0,0 +1,7 @@
+alter table "public"."user_skills" drop column "skill_level";
+
+alter table "public"."users" drop column "MasteryLevel";
+
+alter table "public"."users" drop column "PubSpeakLvl";
+
+
