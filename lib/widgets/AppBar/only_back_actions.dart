2acd7f4ec1cbf6d8a4881a32d1758c58901cd0a9
cozysharkmurks <wilfredalternate6969@gmail.com>
2025-11-23 23:56:04 +0800

    Update only_back_actions.dart

--- lib/widgets/AppBar/only_back_actions.dart
+++ lib/widgets/AppBar/only_back_actions.dart
@@ -9,57 +9,57 @@ class OnlyBackActions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const double buttonSize = 60.0; 
+    const double buttonSize = 60.0;
     const double visibleBarHeight = 80;
 
     return Positioned(
-            top: visibleBarHeight - (buttonSize / 2),
-            left: 20,
-            right: 20,
-            height: buttonSize, 
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                IconButton.filled(
-                  onPressed: () {
-                    if (Navigator.canPop(context)) {
-                      Navigator.pop(context);
-                    }
-                  },
-                  icon: const Icon(Icons.arrow_back),
-                  iconSize: 50, 
-                  style: IconButton.styleFrom(
-                    backgroundColor: "7962A5".toColor(),
-                    foregroundColor: Colors.white,
-                  ),
-                ),
-
-                Row(
-                  children: [
-                    IconButton.filled(
-                      onPressed: () => logger.d('profile pressed!'),
-                      icon: const Icon(Icons.person),
-                      iconSize: 50,
-                      style: IconButton.styleFrom(
-                        backgroundColor: "7962A5".toColor(),
-                        foregroundColor: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    IconButton.filled(
-                      onPressed: () => logger.d('settings pressed!'),
-                      icon: const Icon(Icons.settings),
-                      iconSize: 50,
-                      style: IconButton.styleFrom(
-                        backgroundColor: "7962A5".toColor(),
-                        foregroundColor: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+      top: visibleBarHeight - (buttonSize / 2),
+      left: 20,
+      right: 20,
+      height: buttonSize,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          IconButton.filled(
+            onPressed: () {
+              if (Navigator.canPop(context)) {
+                Navigator.pop(context);
+              }
+            },
+            icon: const Icon(Icons.arrow_back),
+            iconSize: 50,
+            style: IconButton.styleFrom(
+              backgroundColor: "7962A5".toColor(),
+              foregroundColor: Colors.white,
             ),
-          );
+          ),
+
+          Row(
+            children: [
+              IconButton.filled(
+                onPressed: () => logger.d('profile pressed!'),
+                icon: const Icon(Icons.person),
+                iconSize: 50,
+                style: IconButton.styleFrom(
+                  backgroundColor: "7962A5".toColor(),
+                  foregroundColor: Colors.white,
+                ),
+              ),
+              const SizedBox(width: 10),
+              IconButton.filled(
+                onPressed: () => logger.d('settings pressed!'),
+                icon: const Icon(Icons.settings),
+                iconSize: 50,
+                style: IconButton.styleFrom(
+                  backgroundColor: "7962A5".toColor(),
+                  foregroundColor: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
